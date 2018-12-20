f7c3e150e9c1b68e41cacd6fa583d121d0b44295
aleluc91 <aleluc.bsn@gmail.com>
2018-12-20 18:00:01 +0100

    Created constructor for creatinf a matrix with all values equal to zero, or with random values. Closes #2

--- src/matrix.py
+++ src/matrix.py
@@ -2,8 +2,34 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+@author Alessio Lucarella
 """
+
+import random
 
 class Matrix:
     
+    def __init__(self, n, m, gen = "random"):
+        self.matrix = []
+        self.n = n
+        self.m = m
+        for i in range(0, n):
+            if gen == "random":
+                self.matrix.append(self.__generate_rows_of_rand_number)
+            elif gen == "zeros":
+                self.matrix.append(self.__generate_rows_of_zeros)
+            
+    def __generate_rows_of_zeros(self, m):
+        row = []
+        for i in range(0, m):
+            row.append(0)
+        return row
+    
+    def __generate_rows_of_rand_number(self, m):
+        row = []
+        for i in range(0, m):
+            row.append(random.randrange(0, 100))
+        return row
+    
+    
+matrix = Matrix(2,2)
