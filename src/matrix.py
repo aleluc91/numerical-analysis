8f703f8cad317a604c44a54201d8ad30aab812b0
aleluc91 <aleluc.bsn@gmail.com>
2018-12-20 18:20:06 +0100

    Implemented __str__ method for the Matrix class. Closed #3

--- src/matrix.py
+++ src/matrix.py
@@ -15,9 +15,9 @@ class Matrix:
         self.m = m
         for i in range(0, n):
             if gen == "random":
-                self.matrix.append(self.__generate_rows_of_rand_number)
+                self.matrix.append(self.__generate_rows_of_rand_number(m))
             elif gen == "zeros":
-                self.matrix.append(self.__generate_rows_of_zeros)
+                self.matrix.append(self.__generate_rows_of_zeros(m))
             
     def __generate_rows_of_zeros(self, m):
         row = []
@@ -31,5 +31,17 @@ class Matrix:
             row.append(random.randrange(0, 100))
         return row
     
+    def __str__(self):
+        result = ""
+        for i in range(0, self.n):
+            result += "["
+            for j in range(0, self.m):
+                result += str(self.matrix[i][j])
+                if j != self.m - 1:
+                    result += " "
+            result += "]\n"
+        return result
     
-matrix = Matrix(2,2)
+    
+matrix = Matrix(2, 2)
+print(matrix)
