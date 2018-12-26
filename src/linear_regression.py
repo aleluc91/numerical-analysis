be84740f353349ee47216908f27e60a9c1b31a9e
aleluc91 <aleluc.bsn@gmail.com>
2018-12-26 20:22:52 +0100

    Created a function to read from a csv file. Closes #15

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -6,3 +6,20 @@ Created on Wed Dec 26 12:09:59 2018
 @author: aleluc
 """
 
+import csv
+#import pandas
+#import os
+
+def read_csv(f):
+    rows = []
+    with open(f, newline = '')  as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        for row in csv_reader:
+            rows.append(row)
+    return rows
+
+print(read_csv("/home/aleluc/Desktop/Programming/numerical-analysis/src/test1.csv"))
+
+#data = pandas.read_csv("/home/aleluc/Desktop/Programming/numerical-analysis/src/test1.csv")
+
+            
